03bc13d46a3d329dac9c8ed66abc3f1139c92e5c
Charlie May <61992595+charlie-may86@users.noreply.github.com>
2022-06-07 07:16:47 -0500

    just getting started

---
+++ src/components/AboutMe.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <div>
+      <p>
+        I am a driven full stack software engineer looking to take the next step
+        in my career. My focus is on modern technologies, and I have experience
+        working with JavaScript, React, and Node.js. I am a lifelong learner and
+        problem solver at heart.
+      </p>
+    </div>
+  );
+};
+
+export default AboutMe;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <h2>Charlie May</h2>
+        <h2>Full Stack Software Engineer</h2>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
